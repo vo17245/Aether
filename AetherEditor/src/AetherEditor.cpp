5b1f41e9e6dbde16050917b1da09076ae4a65dc4
vo17245 <87613099+vo17245@users.noreply.github.com>
2025-08-29 19:10:01 +0800

    update AetherEditor

--- AetherEditor/src/AetherEditor.cpp
+++ AetherEditor/src/AetherEditor.cpp
@@ -12,56 +12,218 @@
 #include <Debug/Log.h>
 #include "Material.h"
 using namespace Aether;
-
-
-static void DrawMaterialDataItem(MaterialData& data, int id)
+class MaterialDataSetLimitPanel
 {
-    ImGui::Text("%s", data.name.c_str());
-    ImGui::PushID(id);
-    switch (data.type)
+public:
+    MaterialDataSetLimitPanel(MaterialData* data) : m_Data(data)
     {
-    case MaterialDataType::Float: {
-        // slider
-        auto& d = static_cast<MaterialFloat&>(data);
-
-        ImGui::SliderFloat("x", &d.value, d.min, d.max);
     }
-    break;
-    default:
-        break;
-    case MaterialDataType::Vec2: {
-        auto& d = static_cast<MaterialVec2&>(data);
-        ImGui::SliderFloat("x", d.value.data(), d.min.x(), d.max.x());
-        ImGui::SliderFloat("y", d.value.data() + 1, d.min.y(), d.max.y());
-    }
-    break;
-    case MaterialDataType::Vec3: {
-        auto& d = static_cast<MaterialVec3&>(data);
-        ImGui::SliderFloat("x", d.value.data(), d.min.x(), d.max.x());
-        ImGui::SliderFloat("y", d.value.data() + 1, d.min.y(), d.max.y());
-        ImGui::SliderFloat("z", d.value.data() + 2, d.min.z(), d.max.z());
-    }
-    break;
-    case MaterialDataType::Vec4: {
-        auto& d = static_cast<MaterialVec4&>(data);
-        ImGui::SliderFloat("x", d.value.data(), d.min.x(), d.max.x());
-        ImGui::SliderFloat("y", d.value.data() + 1, d.min.y(), d.max.y());
-        ImGui::SliderFloat("z", d.value.data() + 2, d.min.z(), d.max.z());
-        ImGui::SliderFloat("w", d.value.data() + 3, d.min.w(), d.max.w());
-    }
-    break;
-    }
-    ImGui::PopID();
-}
-struct MaterialPanel
-{
-    void Draw(Material& material)
+    void Draw(int id)
     {
-        for (size_t i = 0; i < material.datas.size(); ++i)
+        if (!m_Open)
         {
-            DrawMaterialDataItem(*material.datas[i], i);
+            return;
+        }
+        ImGui::PushID(id);
+
+        ImGui::PopID();
+    }
+
+private:
+    MaterialData* m_Data = nullptr;
+    bool m_Open = false;
+};
+class MaterialDataPanel
+{
+public:
+    MaterialDataPanel(MaterialData* data) : m_Data(data)
+    {
+    }
+    void Draw(int id)
+    {
+        ImGui::Text("%s", m_Data->name.c_str());
+        ImGui::PushID(id);
+        DrawMaterialDataValue(*m_Data);
+        ImGui::PopID();
+    }
+
+private:
+    void DrawMaterialDataValue(MaterialData& data)
+    {
+        switch (data.type)
+        {
+        case MaterialDataType::Float: {
+            // slider
+            auto& d = static_cast<MaterialFloat&>(data);
+
+            ImGui::SliderFloat("x", &d.value, d.min, d.max);
+        }
+        break;
+
+        case MaterialDataType::Vec2: {
+            auto& d = static_cast<MaterialVec2&>(data);
+            ImGui::SliderFloat("x", d.value.data(), d.min.x(), d.max.x());
+            ImGui::SliderFloat("y", d.value.data() + 1, d.min.y(), d.max.y());
+        }
+        break;
+        case MaterialDataType::Vec3: {
+            auto& d = static_cast<MaterialVec3&>(data);
+            ImGui::SliderFloat("x", d.value.data(), d.min.x(), d.max.x());
+            ImGui::SliderFloat("y", d.value.data() + 1, d.min.y(), d.max.y());
+            ImGui::SliderFloat("z", d.value.data() + 2, d.min.z(), d.max.z());
+        }
+        break;
+        case MaterialDataType::Vec4: {
+            auto& d = static_cast<MaterialVec4&>(data);
+            ImGui::SliderFloat("x", d.value.data(), d.min.x(), d.max.x());
+            ImGui::SliderFloat("y", d.value.data() + 1, d.min.y(), d.max.y());
+            ImGui::SliderFloat("z", d.value.data() + 2, d.min.z(), d.max.z());
+            ImGui::SliderFloat("w", d.value.data() + 3, d.min.w(), d.max.w());
+        }
+        break;
+        default:
+            assert(false && "unknown type");
+            break;
         }
     }
+
+private:
+    MaterialData* m_Data = nullptr;
+};
+class AddMaterialDataPanel
+{
+public:
+    AddMaterialDataPanel()
+    {
+        m_Data = CreateScope<MaterialFloat>();
+        m_LastType = MaterialDataType::Float;
+    }
+    void Draw()
+    {
+        if (!m_Open)
+        {
+            return;
+        }
+        // get parent window size
+        ImGui::Begin("Add Material Data", &m_Open);
+        if (ImGui::InputText("Name", m_Name, sizeof(m_Name)))
+        {
+            m_Data->name = m_Name;
+        }
+        ImGui::Combo("Type", (int*)&m_TypeIndex, "Float\0Vec2\0Vec3\0Vec4\0");
+        MaterialDataType type = TypeIndexToEnum(m_TypeIndex);
+        if (type != m_LastType)
+        {
+            m_LastType = type;
+            switch (type)
+            {
+            case MaterialDataType::Float:
+                m_Data = CreateScope<MaterialFloat>();
+                break;
+            case MaterialDataType::Vec2:
+                m_Data = CreateScope<MaterialVec2>();
+                break;
+            case MaterialDataType::Vec3:
+                m_Data = CreateScope<MaterialVec3>();
+                break;
+            case MaterialDataType::Vec4:
+                m_Data = CreateScope<MaterialVec4>();
+                break;
+            default:
+                assert(false && "unknown type");
+                m_Data = CreateScope<MaterialFloat>();
+                break;
+            }
+        }
+        if (ImGui::Button("Add"))
+        {
+            m_Open = false;
+            if (m_OnAdd)
+            {
+                m_Data->name = m_Name;
+                m_OnAdd(*m_Data);
+            }
+        }
+        ImGui::End();
+    }
+    MaterialDataType TypeIndexToEnum(int index)
+    {
+        switch (index)
+        {
+        case 0:
+            return MaterialDataType::Float;
+        case 1:
+            return MaterialDataType::Vec2;
+        case 2:
+            return MaterialDataType::Vec3;
+        case 3:
+            return MaterialDataType::Vec4;
+        default:
+            assert(false && "unknown type");
+            return MaterialDataType::Float;
+        }
+    }
+    void SetOnAdd(std::function<void(MaterialData&)>&& onAdd)
+    {
+        m_OnAdd = std::move(onAdd);
+    }
+    void Open()
+    {
+        m_Open = true;
+    }
+
+private:
+    Scope<MaterialData> m_Data;
+    MaterialDataType m_LastType;
+    int m_TypeIndex = 0;
+    char m_Name[128] = "NewData";
+    std::function<void(MaterialData&)> m_OnAdd;
+    bool m_Open = false;
+};
+class MaterialPanel
+{
+public:
+    void Draw()
+    {
+        for (size_t i = 0; i < m_DataPanels.size(); ++i)
+        {
+            m_DataPanels[i].Draw((int)i);
+        }
+        if (ImGui::Button("Add"))
+        {
+            addPanel.Open();
+        }
+        addPanel.Draw();
+    }
+    MaterialPanel()
+    {
+        addPanel.SetOnAdd([this](MaterialData& data) {
+            switch (data.type)
+            {
+            case MaterialDataType::Float:
+                m_Material.datas.push_back(CreateScope<MaterialFloat>(static_cast<MaterialFloat&>(data)));
+                break;
+            case MaterialDataType::Vec2:
+                m_Material.datas.push_back(CreateScope<MaterialVec2>(static_cast<MaterialVec2&>(data)));
+                break;
+            case MaterialDataType::Vec3:
+                m_Material.datas.push_back(CreateScope<MaterialVec3>(static_cast<MaterialVec3&>(data)));
+                break;
+            case MaterialDataType::Vec4:
+                m_Material.datas.push_back(CreateScope<MaterialVec4>(static_cast<MaterialVec4&>(data)));
+                break;
+            default:
+                assert(false && "unknown type");
+                break;
+            }
+            m_DataPanels.push_back(MaterialDataPanel(m_Material.datas.back().get()));
+        });
+    }
+
+private:
+    AddMaterialDataPanel addPanel;
+    Material m_Material;
+    std::vector<MaterialDataPanel> m_DataPanels;
 };
 class ImGuiLayer : public Layer
 {
@@ -73,12 +235,6 @@ public:
     virtual void OnAttach(Window* window) override
     {
         m_Window = window;
-        m_Material.datas.push_back(CreateScope<MaterialFloat>());
-        m_Material.datas.back()->name = "Roughness";
-        static_cast<MaterialFloat*>(m_Material.datas.back().get())->value = 0.5f;
-        m_Material.datas.push_back(CreateScope<MaterialVec3>());
-        m_Material.datas.back()->name = "BaseColor";
-        static_cast<MaterialVec3*>(m_Material.datas.back().get())->value = Vec3f(1.0f, 0.0f, 0.0f);
     }
     virtual void RegisterRenderPasses(RenderGraph::RenderGraph& renderGraph) override
     {
@@ -100,14 +256,13 @@ public:
 
         ImGui::Begin("Test Window", nullptr, window_flags);
         ImGui::Text("Hello from ImGuiLayer");
-        m_MaterialPanel.Draw(m_Material);
+        m_MaterialPanel.Draw();
         ImGui::End();
     }
 
 private:
     bool m_NeedRebuild;
     Window* m_Window = nullptr;
-    Material m_Material;
     MaterialPanel m_MaterialPanel;
     float m_Float;
 };
